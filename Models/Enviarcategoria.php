c2017efb10974491a308d32bce2390f2aaba8c0e
Duarte551 <99897875+Duarte551@users.noreply.github.com>
2022-05-03 15:40:43 -0300

    Delete Models directory

--- Models/Enviarcategoria.php
+++
@@ -1,7 +0,0 @@
-<?php
-include('Conexaodb.php');
-
-	$nome_categoria = $_POST['nome_categoria'];
-
-	$insereDados = mysqli_query($conn, "INSERT INTO categorias (nome_categoria) VALUES ('$nome_categoria')");
-	echo "<script> alert('Categoria Cadastrada com Sucesso...'); window.location='/Minha-Lojinha/Cadastrarcategoria.php'</script>";

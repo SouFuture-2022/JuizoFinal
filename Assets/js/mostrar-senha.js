5ca90438bb5ba25b0b0561f090319c9a00fcfb13
LucasMaciel404 <99898217+LucasMaciel404@users.noreply.github.com>
2022-05-03 16:06:26 -0300

    Add files via upload

---
+++ Assets/js/mostrar-senha.js
@@ -0,0 +1,9 @@
+function mouseoverPass(obj) {
+	var obj = document.getElementById('senha');
+	obj.type = "text";
+}
+
+function mouseoutPass(obj) {
+	var obj = document.getElementById('senha');
+	obj.type = "password";
+}

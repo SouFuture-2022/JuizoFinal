06e0fa61116190bba8b2c32188216f72711c8b10
LucasMaciel404 <maciellucas@gmail.com>
2022-05-03 15:41:24 -0300

    Lucas 03/05

---
+++ Models/Avaliacoes.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Models;
+
+class Avaliacoes{
+
+	private $estrela;
+	private $id_produto;
+
+	public function setEstrela($estrela) {
+		$this->estrela = $estrela;
+	}
+
+	public function getEstrela() {
+		return $this->estrela;
+	}
+
+	public function setIdproduto($id_produto) {
+		$this->id_produto = $id_produto;
+	}
+	
+	public function getIdproduto() {
+		return $this->id_produto;
+	}
+
+
+}

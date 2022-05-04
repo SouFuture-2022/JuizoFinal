b65ac850b9300cd2dc67774a02db7925ce3f0db6
LucasMaciel404 <maciellucas@gmail.com>
2022-05-04 17:12:20 -0300

    Refatoracao do bootstrap do CadastrarEndereco

--- Views/CadastrarEndereco.php
+++ Views/CadastrarEndereco.php
@@ -28,59 +28,67 @@
 		}
 	}
 ?>
-
+<!--ESSA PAGINA, POR ENQUANTO, SO FUNCIONA SE FOR UTILIZADA COMO UMA BOX. SENÃO, VAI PRECISAR SER REFATORADA.-->
 <section class="section-name bg padding-y-sm">
-	<div class="container">
-	<header class="section-heading">
-		<h3 class="section-title">Cadastrar Endereços</h3>
-	</header>
-	</div>
+    <div class="d-flex justify-content-center">
+        <header class="section-heading">
+            <h3 class="section-title">Cadastrar Endereços</h3>
+        </header>
+    </div>
 </section>
 
-<section class="section-name padding-y">
-	<div class="container">
-		<div class="box">
-			<form action="" method="POST" enctype="multipart/form-data">
-				<div class="form-row">
-					<div class="col-md-6">
-						<div class="form-group">
-							<input type="text" name="numero" value="000" id="numero" placeholder="N°" class="form-control" required />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="form-group">
-							<input type="text" name="cep" value="000000-000" id="cep" placeholder="CEP" maxlength="9" onblur="pesquisacep(this.value);" class="form-control" required />
-						</div>
-					</div>
-				</div>
-				<div class="form-row">
-					<div class="col-md-6">
-						<div class="form-group">
-							<input type="text" name="rua" value="Teste" id="rua" placeholder="Rua" class="form-control" required />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="form-group">
-							<input type="text" name="bairro" value="Teste" id="bairro" placeholder="Bairro" class="form-control" required />
-						</div>
-					</div>
-				</div>
-				<div class="form-row">
-					<div class="col-md-6">
-						<div class="form-group">
-							<input type="text" name="cidade" value="Teste" id="cidade" placeholder="Cidade" class="form-control" required />
-						</div>
-					</div>
-					<div class="col-md-6">
-						<div class="form-group">
-							<input type="text" name="uf" value="CE" id="uf" placeholder="UF" class="form-control" required />
-						</div>
-					</div>
-				</div>
-				<div class="form-group">
-					<button type="submit" name="btCadastrar" class="btn btn-primary btn-block">Registrar</button>
-				</div>
-			</form>
-		</div>
-	</div>
-</section>
+
+<div class="d-flex justify-content-center">
+    <div class="box">
+        <form action="" method="POST" enctype="multipart/form-data">
+            <div class="form-row">
+                <div class="col-md-6 d-flex flex-column pb-2">
+                    <div class="form-group">
+                        <!--<label>CEP</label>-->
+                        <input type="text" name="cep" id="cep" placeholder="CEP" maxlength="9"
+                            onblur="pesquisacep(this.value);" class="form-control" required>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col-md-6 d-flex flex-column pb-2">
+                    <div class="form-group">
+                        <!--<label>Rua</label>-->
+                        <input type="text" name="rua" id="rua" placeholder="Rua" class="form-control" required>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex flex-column pb-2">
+                    <div class="form-group">
+                        <!--<label>Numero</label>-->
+                        <input type="text" name="numero" id="numero" placeholder="N°" class="form-control" required>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex flex-column pb-2">
+                    <div class="form-group">
+                        <!--<label>Bairro</label>-->
+                        <input type="text" name="bairro" id="bairro" placeholder="Bairro" class="form-control"
+                            required>
+                    </div>
+                </div>
+            </div>
+            <div class="form-row">
+                <div class="col-md-6 d-flex flex-column pb-2">
+                    <div class="form-group">
+                        <!--<label>Cidade</label>-->
+                        <input type="text" name="cidade" id="cidade" placeholder="Cidade" class="form-control"
+                            required>
+                    </div>
+                </div>
+                <div class="col-md-6 d-flex flex-column pb-2">
+                    <div class="form-group">
+                        <!--<label>UF</label>-->
+                        <input type="text" name="uf" id="uf" placeholder="UF" class="form-control" required />
+                    </div>
+                </div>
+            </div>
+            <div class="form-group d-flex flex-column">
+                <button type="submit" name="btCadastrar" class="btn btn-primary btn-block">Registrar</button>
+            </div>
+        </form>
+    </div>
+</div>

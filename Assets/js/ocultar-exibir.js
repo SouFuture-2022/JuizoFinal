cb422e8297c31e2559540c5f4c864eb055f1b8c6
Duarte <joaovictordfs551@gmail.com>
2022-05-04 16:36:49 -0300

    Teste

--- Assets/js/ocultar-exibir.js
+++
@@ -1,7 +0,0 @@
-function mostrar(id){
-	if(document.getElementById(id).style.display == 'none'){
-		document.getElementById(id).style.display = 'block';
-	}else{
-		document.getElementById(id).style.display = 'none';
-	}
-}

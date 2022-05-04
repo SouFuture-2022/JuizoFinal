dc3584083269afcb47dd433c45a3de710235ef61
Duarte <joaovictordfs551@gmail.com>
2022-05-04 16:14:41 -0300

    Apagado com sucesso

--- Assets/js/mostrar-senha.js
+++
@@ -1,9 +0,0 @@
-function mouseoverPass(obj) {
-	var obj = document.getElementById('senha');
-	obj.type = "text";
-}
-
-function mouseoutPass(obj) {
-	var obj = document.getElementById('senha');
-	obj.type = "password";
-}
--- Controllers/FinalizarCompra.php
+++
@@ -1,2 +0,0 @@
-<?php
-class FinalizarCompra extends Controller {}

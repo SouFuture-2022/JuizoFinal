b2def4bc30efee3b9f4747a5791b6165ffaa81ba
Daniel <S0us4.dan@gmail.com>
2022-05-04 15:27:02 -0300

    Refatorando o model de Tamanhos

--- Infra/Dao/Tamanhos/AlterarTamanhosDb.php
+++ Infra/Dao/Tamanhos/AlterarTamanhosDb.php
@@ -1,1 +1,23 @@
-asdasd
+<?php
+
+namespace Infra\Dao\Tamanhos;
+
+use Infra\Database\Conexao;
+
+class  AlterarTamanhosDb {
+    #id_produto estava sendo passado como parametro na função update, gerando um erro no bind param
+
+
+    public function update($id_tamanho) {
+        $db = new Conexao();
+
+		$sql  = "UPDATE tamanhos SET sub_categoria = :sub_categoria, tamanho_superior = :tamanho_superior, tamanho_inferior = :tamanho_inferior, quantidade_tamanho = :quantidade_tamanho WHERE id_tamanho = :id_tamanho";
+		$stmt = $db->Conexao->prepare($sql);
+		$stmt->bindParam(':sub_categoria', $this->sub_categoria);
+		$stmt->bindParam(':tamanho_superior', $this->tamanho_superior);
+		$stmt->bindParam(':tamanho_inferior', $this->tamanho_inferior);
+		$stmt->bindParam(':quantidade_tamanho', $this->quantidade_tamanho);
+		$stmt->bindParam(':id_tamanho', $id_tamanho);
+		return $stmt->execute();
+	}
+}
--- Infra/Dao/Tamanhos/CadastrarTamanhosDb.php
+++ Infra/Dao/Tamanhos/CadastrarTamanhosDb.php
@@ -1,1 +1,21 @@
-asdasd
+<?php
+
+namespace Infra\Dao\Tamanhos;
+use Infra\Database\Conexao;
+
+class CadastrarTamanhosDb {
+
+    public function insert(){
+        $db = new Conexao();
+
+		$sql = "INSERT INTO tamanhos (sub_categoria, tamanho_superior, tamanho_inferior, quantidade_tamanho, id_produto, criado_em) VALUES (:sub_categoria, :tamanho_superior, :tamanho_inferior, :quantidade_tamanho, :id_produto, NOW()";
+		$stmt = $db->Conexao->prepare($sql);
+		$stmt->bindParam(':sub_categoria', $this->sub_categoria);
+		$stmt->bindParam(':tamanho_superior', $this->tamanho_superior);
+		$stmt->bindParam(':tamanho_inferior', $this->tamanho_inferior);
+		$stmt->bindParam(':quantidade_tamanho', $this->quantidade_tamanho);
+		$stmt->bindParam(':id_produto', $this->id_produto);
+		return $stmt->execute();
+	}
+
+}

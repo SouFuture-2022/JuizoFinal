327490c2fee888652c0acde248448b9f1d741507
ASCland4 <94983399+ASCland4@users.noreply.github.com>
2022-05-10 17:22:17 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,35 +1,38 @@
 <?php
 
-use Models\Categorias;
+use App\Models\Categorias;
 
-	session_start();
-	#$categoria = new Categorias();
 
-	$menu = 0;
-	if($menu == 0) {
-		include('/home/daniel/juizo_final/JuizoFinal/Cabecalhos/menu.php');
-	} elseif($menu == 1) {
-		include('/home/daniel/juizo_final/JuizoFinal/Cabecalhos/menucliente.php');
-	} else {
-		include('./Cabecalhos/menuadmin.php');
-	}
+session_start();
+//$categoria = new Categorias();
+$menu = 0;
+if ($menu == 0) {
+	include('includes/Cabecalhos/menu.php');
+} elseif ($menu == 1) {
+	include('includes/Cabecalhos/menucliente.php');
+} else {
+	include('includes/Cabecalhos/menuadmin.php');
+}
 
-	include('Rotas.php');
-	//	Redenriza o corpo da página.
-	#function __autoload($class_name) {
-		if(file_exists('./Models/' . $class_name . '.php')) {
-			include('./Models/' . $class_name . '.php');
-		} elseif(file_exists('./Controllers/' . $class_name . '.php')) {
-			include('./Controllers/' . $class_name . '.php');
-		}
-	#}
+include('Rotas.php');
+//	Redenriza o corpo da página.
+function __autoload($class_name) {
+if (file_exists('App/Models/' . $class_name . '.php')) {
+	include('App/Models/' . $class_name . '.php');
+} elseif (file_exists('App/Controllers/' . $class_name . '.php')) {
+	include('App/Controllers/' . $class_name . '.php');
+}
+}
 
-	$rodape = 0;
-	if($rodape == 0) {
-		include('./Rodapes/rodape.php');
-	} elseif($rodape == 1) {
-		include('./Rodapes/rodape.php');
-	} else {
-		include('./Rodapes/rodapeadmin.php');
-	}
-?>
+$rodape = 0;
+if ($rodape == 0) {
+	include('includes/rodape.php');
+} elseif ($rodape == 1) {
+	include('includes/rodape.php');
+} else {
+	include('includes/rodapeadmin.php');
+}
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+rota();

69f99738b52ac969c317951bd005e01cfae0d0b4
TamasJonhson <100958931+TamasJonhson@users.noreply.github.com>
2022-05-11 16:50:38 -0300

    Add files via upload

---
+++ Views/CadastrarCategoria.php
@@ -0,0 +1,23 @@
+
+
+<section class="section-name bg padding-y-sm">
+	<div class="container">
+	<header class="section-heading">
+		<h3 class="section-title">Cadastrar Categoria</h3>
+	</header>
+	</div>
+</section>
+<section class="section-name padding-y">
+	<div class="container">
+		<div class="box">
+			<form action="" method="POST" enctype="multipart/form-data">
+				<div class="form-group">
+					<input type="text" name="nome_categoria" id="nomeCategoria" placeholder="Nome da Categotia" class="form-control" required />
+				</div>
+				<div class="form-group">
+					<button type="submit" name="btCadastrar" class="btn btn-primary btn-block">Registrar</button>
+				</div>
+			</form>
+		</div>
+	</div>
+</section>
---
+++ Views/Contato.php
@@ -0,0 +1,32 @@
+<section class="section-name bg padding-y-sm">
+<div class="container">
+<header class="section-heading">
+	<h3 class="section-title">Contate-nos</h3>
+</header>
+</div>
+</section>
+
+<section class="section-name padding-y">
+	<div class="container">
+		<article class="card mb-4">
+			<div class="box">
+				<div class="row">
+					<div class="col-md-6">
+						<ul>
+							<li>Horário de Funcionamento</li>
+							<li>08:00hs ás 12:00hs</li>
+							<li>13:00hs ás 17:00hs</li>
+						</ul>
+					</div>
+					<div class="col-md-6">
+						<ul>
+							<li>+55 (00) 9.0000-0000 </li>
+							<li>+55 (00) 3336-1234</li>
+							<li>info@lojademooficial.com</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</article>
+	</div>
+</section>

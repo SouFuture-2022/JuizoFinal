4ac8ee090455b72e4204d431aa37d3bb42b04b62
Helena <helenabraz94@gmail.com>
2022-05-16 16:08:43 -0300

    Refatoração do CadastrarEndereço e ajuste do Login e CadastrarUsuario

--- Views/Login.php
+++ Views/Login.php
@@ -1,34 +1,26 @@
-<!doctype html>
-<html lang="pt-br">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Login</title>
-</head>
 
-<body>
+<section>
     <div class="container">
         <div class="row d-flex justify-content-center">
             <div class="col-lg-4 col-md-6">
                 <div class="card shadow p-3 mb-5 bg-body rounded">
                     <div class="card-body">
-                        <h2>Entrar</h2>
+                        <h2 class="text-primary ">Entrar</h2>
 
                         <form method="post">
                             <div class="mb-3">
                                 <div class="input-group mb-3">
-                                    <span class="input-group-text"><img src="Assets/images/usuario.png" alt="icone de perfil"></span>
-                                    <input type="email" name="email" class="form-control" placeholder="Email">
+                                    <span class="input-group-text"><img src="../Assets/images/usuario.png" alt="icone de perfil"></span>
+                                    <input type="email" name="email" class="form-control" placeholder="Email" required>
                                 </div>
                             </div>
 
                             <div class="mb-3">
                                 <div class="input-group mb-3">
-                                    <span class="input-group-text"><img src="Assets/images/cadeado.png" alt="icone de senha"></span>
-                                    <input type="password" name="email" class="form-control" placeholder="Senha">
+                                    <span class="input-group-text"><img src="../Assets/images/cadeado.png" alt="icone de senha"></span>
+                                    <input type="password" name="email" class="form-control" placeholder="Senha" required>
                                 </div>
                             </div>
 
@@ -60,6 +52,4 @@
             </div>
         </div>
     </div>
-</body>
-
-</html>
+</section>

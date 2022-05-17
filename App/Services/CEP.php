fb9e6b56d96945a6bc5069d91144a390216ad02c
LucasMaciel404 <maciellucas@gmail.com>
2022-05-17 13:58:35 -0300

    para pegar a cidade etc

---
+++ App/Services/CEP.php
@@ -0,0 +1,12 @@
+<?php 
+/*
+ * Funçao feita para poder pegar as informaçoes e endereço do cliente;
+ * Endereço, Cidade etc;
+ */
+function get_infor_Cep ($cep){
+
+    $endpoint  = "https://viacep.com.br/ws/$cep/json/";
+    $json = file_get_contents($endpoint);
+    $info = json_decode($json, true);
+    return $info;
+}

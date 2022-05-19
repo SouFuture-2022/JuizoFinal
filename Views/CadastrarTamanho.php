7684cd7a51baa056bdb27216a399fd42bb86eeea
ASCland4 <94983399+ASCland4@users.noreply.github.com>
2022-05-19 17:03:35 -0300

    Upload de mudanças

--- Views/CadastrarTamanho.php
+++ Views/CadastrarTamanho.php
@@ -1,5 +1,61 @@
-<?php
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+<section>
+    <div class="container d-flex justify-content-center">
+        <div class="card shadow p-3 mb-5 bg-body rounded w-50 p-3">
+            <div class="card-body">
+                <h2 class="text-primary mb-3">Cadastrar tamanho</h2>
+                <form>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <select class="form-select text-muted" aria-label="Default select example">
+                                <option selected>Sub categorias</option>
+                                <option value="1">Calças</option>
+                                <option value="2">Calçados</option>
+                                <option value="3">Camisas</option>
+                            </select>
+                        </div>
+                        <!--OS PRODUTOS SÃO INFORMAÇÕES QUE ESTÃO CADASTRADAS NO BANCO DE DADOS, LOGO, CABE AO BACK, QUE
+                        A GENTE TANTO AMA, RESOLVER ESSE PROBLEMA sz-->
+                        <div class="col">
+                            <select class="form-select text-muted" aria-label="Default select example">
+                                <option selected>Produto</option>
+                                <option value="1">Produto 1</option>
+                                <option value="2">Produto 2</option>
+                                <option value="3">Produto 3</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <select class="form-select text-muted" aria-label="Default select example">
+                                <option selected>Tamanho superior</option>
+                                <option value="P">P</option>
+                                <option value="M">M</option>
+                                <option value="G">G</option>
+                                <option value="GG">GG</option>
+                            </select>
+                        </div>
+                        <!--A QUANTIDADE ESTÁ EM TIPO TEXT POR MOTIVOS ESTETICOS. CABE AO BACK, QUE A GENTE TANTO AMA,
+                        NAO PERMITIR A ENTRADA DE CARACTERES, APENAS NUMEROS.-->
 
+                        <!--O TAMANHO INFERIOR OU SUPERIOR NÃO PODE SER OBRIGATÓRIAMENTE REQUERIDO, PARTINDO DA IDEIA DE
+                        QUE UM TAMANHO SUPERIOR NAO PRECISA TER UM TAMANHO INFERIOR. EX: UMA BLUSA JA TEM O TAMANHO P, ENTAO
+                        ELA NAO PODE TER TAMANHO 45 AO MESMO TEMPO-->
+                        <div class="col">
+                            <input type="text" name="" placeholder="Tamanho infeiror" class="form-control">
+                        </div>
+                    </div>
+                    <div class="d-flex justify-content-center">
+                        <button class="btn w-75 btn-primary">Cadastrar</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<?php
+/*
 use App\Models\Tamanho;
 use App\Models\Produtos;
 use App\Infra\Dao\Tamanhos\CadastrarTamanhosDb;
@@ -26,15 +82,9 @@ if (isset($_POST['btCadastrar'])) {
 	if ($cadastrar_tamanho->insert()) {
 		include('Includes/MsgSucesso.php');
 	}
-}
+}*/
 ?>
 
-<link href="Assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
-<link href="Assets/css/all.min.css" rel="stylesheet" type="text/css">
-<link href="Assets/css/ui.css" rel="stylesheet" type="text/css" />
-<link href="Assets/css/ocultar-exibir.css" type="text/css" rel="stylesheet">
-<link href="Assets/css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
-<link href="Assets/css/avaliacao-estrelas.css" rel="stylesheet" type="text/css" />
 <section class="section-name bg padding-y-sm">
     <div class="container">
         <header class="section-heading">
@@ -88,9 +138,15 @@ if (isset($_POST['btCadastrar'])) {
                     <div class="col-md-6">
                         <select class="form-control" name="id_produto" required>
                             <option value="">Produto</option>
-                            <?php foreach ($listar_produto->findAllSelect() as $key => $value) { ?>
-                            <option value="<?php echo $value->id_produto; ?>"><?php echo $value->nome; ?></option>
-                            <?php } ?>
+                            <? //php foreach ($listar_produto->findAllSelect() as $key => $value) { 
+                            ?>
+                            <option value="<? //php echo $value->id_produto; 
+                                            ?>">
+                                <? //php echo $value->nome; 
+                                ?>
+                            </option>
+                            <? //php } 
+                            ?>
                         </select>
                     </div>
                 </div>

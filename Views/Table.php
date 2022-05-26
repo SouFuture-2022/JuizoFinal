869bcc53ab0785e7495a613f5343aced1ad2517c
LucasMaciel404 <maciellucas@gmail.com>
2022-05-26 13:16:44 -0300

    refatoraçao incompleta

--- Views/Table.php
+++ Views/Table.php
@@ -1,4 +1,79 @@
-<link href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css" rel="stylesheet">
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+
+<section>
+    <div class="container">
+        <div class="card card shadow p-3 mb-5 bg-body rounded">
+            <div class="card-body">
+                <h2 class="text-primary mb-3">Tabela</h2>
+                <div class="row d-flex justify-content-between d-flex align-items-center mb-3">
+                    <div class="col">
+                        <div>
+                            <div class="row d-flex align-items-center">
+                                    Mostrando
+                                    <select style="width: 70px; margin-left: 5px; margin-right: 5px;" class="form-select" aria-label="Default select example">
+                                        <option value="1">5</option>
+                                        <option value="2">10</option>
+                                        <option value="3">15</option>
+                                        <option value="4">20</option>
+                                    </select>
+                                    Registros por página
+                            </div> 
+                        </div>
+                    </div>
+                    <div class="col d-flex justify-content-end">
+                        <div class="input-group input-group">
+                            <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                            <button class="input-group-text">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                </div>
+                <table class="table table-striped">
+                    <thead>
+                      <tr>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Email</th>
+                        <th scope="col">Telefone</th>
+                        <th scope="col">Ação</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      <tr>
+                        <td>Nome 1</td>
+                        <td>Email 1</td>
+                        <td>Telefone 1</td>
+                        <td><a href="#">Deletar</a></td>
+                      </tr>
+                      <tr>
+                        <td>Nome 2</td>
+                        <td>Email 2</td>
+                        <td>Telefone 2</td>
+                        <td><a href="#">Deletar</a></td>
+                      </tr>
+                      <tr>
+                        <td>Nome 3</td>
+                        <td>Email 3</td>
+                        <td>Telefone 3</td>
+                        <td><a href="#">Deletar</a></td>
+                      </tr>
+                    </tbody>
+                </table>
+                <div class="row">
+                    <div class="col">
+                        Mostrando 1 de 1
+                    </div>
+                    <div class="col">
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!--<link href="//cdn.datatables.net/1.10.15/css/jquery.dataTables.min.css" rel="stylesheet">
 <link href="Assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
 <link href="Assets/css/all.min.css" rel="stylesheet" type="text/css">
 <link href="Assets/css/ui.css" rel="stylesheet" type="text/css" />

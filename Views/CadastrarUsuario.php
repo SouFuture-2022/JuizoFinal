afcd6368a8340002f5f7de210d2b3ff34500a38a
Helena <helenabraz94@gmail.com>
2022-06-09 16:50:58 -0300

    atualização do cadastrar e login, e novo arquivo css

--- Views/CadastrarUsuario.php
+++ Views/CadastrarUsuario.php
@@ -40,52 +40,55 @@ if (isset($_POST['btCadastrar'])) {
 }
 ?>
 
-<link href="Assets/css/bootstrap.css" rel="stylesheet" type="text/css" />
-<link href="Assets/css/all.min.css" rel="stylesheet" type="text/css">
-<link href="Assets/css/ui.css" rel="stylesheet" type="text/css" />
-<link href="Assets/css/ocultar-exibir.css" type="text/css" rel="stylesheet">
-<link href="Assets/css/responsive.css" rel="stylesheet" media="only screen and (max-width: 1200px)" />
-<link href="Assets/css/avaliacao-estrelas.css" rel="stylesheet" type="text/css" />
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+<link rel="stylesheet" href="../Assets/css/centralizar.css">
 <section>
-    <div class="container d-flex justify-content-center mt-4">
+    <div>
+        <a href="/" class="btn btn-primary ms-1 mt-1">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-arrow-left-circle" viewBox="0 0 16 16">
+                <path fill-rule="evenodd" d="M1 8a7 7 0 1 0 14 0A7 7 0 0 0 1 8zm15 0A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-4.5-.5a.5.5 0 0 1 0 1H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5z"/>
+            </svg>
+        </a>    
+    </div>  
+    <div class="container d-flex justify-content-center" id="teste">
         <div class="card shadow p-3 mb-5 bg-body rounded w-50 p-3">
             <div class="card-body">
                 <h2 class="mb-3 text-primary">Cadastre-se</h2>
-                <form method="post" action="">
+                <form action="">
                     <div class="row mb-3">
                         <div class="col">
-                            <input type="text" class="form-control" name="nome" placeholder="Nome completo" required>
+                            <input type="text" class="form-control" placeholder="Nome completo" required>
                         </div>
                         <div class="col">
-                            <input type="text" class="form-control" name="perfil" placeholder="Nome de usuário" required>
+                            <input type="text" class="form-control" placeholder="Nome de usuário" required>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col">
-                            <input type="text" class="form-control" name="cpf" placeholder="CPF" required>
+                            <input type="email" class="form-control" placeholder="Email" required>
                         </div>
                         <div class="col">
-                            <input type="text" class="form-control" name="telefone" placeholder="Telefone" required>
-                        </div>
-                    </div>
-                    <div class="row mb-3">
-                    <div class="col">
-                            <input type="date" class="form-control" name="data_nascimento" placeholder="Data de Nascimento" required>
-                        </div>
-                        <div class="col">
-                            <input type="email" class="form-control" name="email" placeholder="Email" required>
+                            <input type="text" class="form-control" placeholder="Telefone" required>
                         </div>
                     </div>
                     <div class="row mb-3">
                         <div class="col">
-                            <input type="password" class="form-control" name="senha" placeholder="Senha" required>
+                            <input type="text" class="form-control" placeholder="CPF" required>
                         </div>
                         <div class="col">
-                            <input type="password" class="form-control" name="senha" placeholder="Confirmar senha" required>
+                            <input type="date" class="form-control text-muted" placeholder="Data de nascimento" required>
+                        </div>
+                    </div>
+                    <div class="row mb-3">
+                        <div class="col">
+                            <input type="password" class="form-control" placeholder="Senha" required>
+                        </div>
+                        <div class="col">
+                            <input type="password" class="form-control" placeholder="Confirmar senha" required>
                         </div>
                     </div>
                     <div class="d-flex justify-content-center">
-                        <input class="btn btn-primary w-75 " type="submit" id="button" name="cadastrar" value="Cadastrar">
+                        <button class="btn w-75 btn-primary">Cadastrar</button> 
                     </div>
                 </form>
                 <hr>
@@ -93,14 +96,12 @@ if (isset($_POST['btCadastrar'])) {
                     <p>Já tem uma conta?</p>
                 </div>
                 <div class="d-flex justify-content-center">
-                    <a href="Login" class="btn w-75 btn-outline-primary" href="#">Entrar</a>
+                    <a class="btn w-75 btn-outline-primary" href="#">Entrar</a>
                 </div>
             </div>
         </div>
     </div>
 </section>
-
-
 <?php  
 
 if (isset($_POST['cadastrar'])){
